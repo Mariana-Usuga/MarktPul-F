673ba4d099ef9a8292ac235bd8524addb148d2ee
Mariana-Usuga <Mariana-Usuga>
2021-11-27 19:02:22 -0500

    Create item detail

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import ItemDetail from '../src/pages/ItemDetail'
+
+const App = () => {
+  return (
+    <>
+    <ItemDetail />
+ <h2>HOLLA</h2>
+ </>
+  )
+}
+
+export default App;
---
+++ src/pages/ItemDetail.jsx
@@ -0,0 +1,13 @@
+// import product from '../styles/image/product.png'
+import '../styles/pages/itemDetail.css'
+
+const ItemDetail = () =>{
+    return(
+      <div>
+        {/* <img src={product}></img> */}
+        working
+      </div>
+    )
+}
+
+export default ItemDetail
