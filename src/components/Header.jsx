c3f8123d4f91061e1c05a301f5bdf8a48a3a7334
Jorge Luis Adco <jorgead0812@gmail.com>
2021-11-30 11:35:05 -0500

    resolviendo conflictos con los estilos de headermain.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,37 +4,37 @@ import '../styles/components/header.scss';
 const Header = () => {
   const handleMenu = () => {
     // const iconoMenu = document.querySelector('.header__main-bars');
-    const menu = document.querySelector('.header__ul');
-    menu.classList.toggle('header__ul--show');
+    const menu = document.querySelector('.search-header__ul');
+    menu.classList.toggle('search-header__ul--show');
   };
   return (
     <header>
-      <nav className="header__nav">
-        <div className="header__main">
-          <h2 className="header__main-logo">MarktPul</h2>
+      <nav className="search-header__nav">
+        <div className="search-header__main">
+          <h2 className="search-header__main-logo">MarktPul</h2>
           {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
           <i
-            className="header__main-bars fas fa-bars"
+            className="search-header__main-bars fas fa-bars"
             onClick={handleMenu}
             onKeyPress={handleMenu}
             role="button"
             tabIndex="0"
           />
         </div>
-        <div className="header__des__d">
+        <div className="search-header__des__d">
           <input
-            className="header__des__d__input"
+            className="search-header__des__d__input"
             type="text"
             placeholder="search for anything"
           />
-          <i className="header__des__d__fa fas fa-search" />
+          <i className="search-header__des__d__fa fas fa-search" />
         </div>
-        <ul className="header__ul">
-          <li className="header__li">Inicio</li>
-          <li className="header__li">Registro</li>
-          <li className="header__li">Mi cuenta</li>
-          <li className="header__li">
-            <i className="header__mobile-cart fas fa-shopping-cart" />
+        <ul className="search-header__ul">
+          <li className="search-header__li">Inicio</li>
+          <li className="search-header__li">Registro</li>
+          <li className="search-header__li">Mi cuenta</li>
+          <li className="search-header__li">
+            <i className="search-header__mobile-cart fas fa-shopping-cart" />
           </li>
         </ul>
       </nav>
