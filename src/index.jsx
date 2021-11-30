7ecc6b43fb36b5d3db4389206f96b0dad244e0ec
JoseCarlos <jocdiazmuic@gmail.com>
2021-11-30 14:27:03 -0500

    Integratioin with react-rout and merge issues

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import Register from './pages/Register';
 
 const Routing = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   </BrowserRouter>
 );
