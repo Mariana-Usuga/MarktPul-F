2c45e1b4864ab5fda99833bebf706a6a34e6cf42
Mariana-Usuga <Mariana-Usuga>
2021-12-18 13:18:11 -0500

    delete console.log

--- src/components/PaymentOnDelivery.jsx
+++ src/components/PaymentOnDelivery.jsx
@@ -1,10 +1,7 @@
-const PaymentOnDelivery = () => {
-  console.log('working');
-  return (
-    <div className="payDelivery">
-      <h3>PAGO CONTRAENTREGA </h3>
-    </div>
-  );
-};
+const PaymentOnDelivery = () => (
+  <div className="payDelivery">
+    <h3>PAGO CONTRAENTREGA </h3>
+  </div>
+);
 
 export default PaymentOnDelivery;
