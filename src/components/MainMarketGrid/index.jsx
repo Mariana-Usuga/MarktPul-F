e07bc3ff79955bc08279fe4d6b164827b9288a57
Jorge Luis Adco <jorgead0812@gmail.com>
2022-01-22 13:34:59 -0500

    solucionando problemas

---
+++ src/components/MainMarketGrid/index.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import ProductItem from '../ProductItem';
+import { fetchMarkets } from '../../store/actions/landingPageActionsCreator';
+import './MainMarketGrid.scss';
+
+const MainProductGrid = () => {
+  const dispatch = useDispatch();
+  const markets = useSelector((state) => state.markets);
+  useEffect(() => {
+    dispatch(fetchMarkets());
+  }, []);
+  return (
+    <div className="container">
+      {markets.map((market) => {
+        const { title, place, image, _id: id } = market;
+        return (
+          <Link to={`/main/marketDetail/${id}`} key={id}>
+            <ProductItem title={title} place={place} image={image} />
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+export default MainProductGrid;
