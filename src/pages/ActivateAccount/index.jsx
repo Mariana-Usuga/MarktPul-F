99bc940414bc93bba2432cd6ada7df26e525d984
JoseCarlos <jocdiazmuic@gmail.com>
2022-01-24 17:53:24 -0500

    completed activation email

--- src/pages/ActivateAccount/index.jsx
+++ src/pages/ActivateAccount/index.jsx
@@ -8,17 +8,24 @@ import './ActivateAccount.scss';
 const ActivateAcount = () => {
   const { hash } = useParams();
   const [apiResponse, setApiResponse] = useState(false);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     const activateAccount = async () => {
-      const res = await axios.post(
-        'https://marktpul-b-temp.herokuapp.com/auth/local/validate-email',
-        {
-          hash: `${hash}`,
-        },
-      );
-      setApiResponse(res.data);
+      try {
+        const res = await axios.post(
+          'https://marktpul-b-temp.herokuapp.com/auth/local/validate-email',
+          {
+            hash: `${hash}`,
+          },
+        );
+        setApiResponse(res.data);
+      } catch (error) {
+        setApiResponse({ message: 'token not found' });
+      } finally {
+        setLoading(false);
+      }
     };
     activateAccount();
   }, []);
@@ -28,8 +35,9 @@ const ActivateAcount = () => {
     if (timer == 0) {
       setTimer(null);
     }
-    if (!timer) {
-      return navigate('/');
+    if (!timer && apiResponse.JWT) {
+      setLoading(true);
+      return navigate('/login');
     }
 
     const intervalId = setInterval(() => {
@@ -41,18 +49,47 @@ const ActivateAcount = () => {
     };
   }, [timer]);
 
-  return (
-    <div className="ActivateAccount">
-      {apiResponse.JWT ? (
+  const handleRedirect = () => {
+    setLoading(true);
+    navigate('/login');
+  };
+
+  const displayMsg = ({ JWT }) => {
+    if (JWT) {
+      return (
         <div>
           <h1>¡Muchas Gracias por activar tu cuenta!</h1>
           <p>{`Serás redirigido al inicio en ${timer || 0} segundos...`}</p>
-          <p>Si no funciona, haz click acá</p>
+          <p>Si no funciona... </p>
+          <button
+            type="button"
+            onClick={handleRedirect}
+            className="activate__login-button"
+          >
+            {' '}
+            Inicia sesión
+          </button>
         </div>
+      );
+    }
+    return (
+      <div>
+        <h1>Algo malo ha sucedido...</h1>
+        <p className="activate__redirect">
+          Token inválido o ha expirado. Comprueba tu email o intenta iniciando
+          sesión.
+        </p>
+      </div>
+    );
+  };
+
+  return (
+    <div className="ActivateAccount">
+      {!loading ? (
+        <div>{displayMsg(apiResponse)}</div>
       ) : (
         <div>
-          <h1>Algo malo ha sucedido...</h1>
-          <p>Token inválido o ha expirado</p>
+          <h1>Validando...</h1>
         </div>
       )}
     </div>
