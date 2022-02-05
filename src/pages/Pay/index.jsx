f9e69d329102d9bbd2393a04bee5cf987826341d
JoseCarlos <jocdiazmuic@gmail.com>
2022-02-05 17:05:43 -0500

    fix layout payment process

--- src/pages/Pay/index.jsx
+++ src/pages/Pay/index.jsx
@@ -11,6 +11,15 @@ const Pay = ({ id, canProceed, setCanProceed }) => {
   return (
     <div className="payContainer">
       <div className="payContainer__item">
+        <ShipmentSummary id={id} />
+        <div className="orderSummary">
+          <div>
+            <h2 className="titleOrder">Resumen de la compra</h2>
+          </div>
+          {id ? <BuyAproduct id={id} /> : <ShoppingCart />}
+        </div>
+      </div>
+      <div className="payContainer__item">
         <div className="paymentMethod">
           <h3>Elige tu medio de pago</h3>
         </div>
@@ -19,15 +28,6 @@ const Pay = ({ id, canProceed, setCanProceed }) => {
           canProceed={canProceed}
           setCanProceed={setCanProceed}
         />
-      </div>
-      <div className="payContainer__item">
-        <ShipmentSummary id={id} />
-        <div className="orderSummary">
-          <div>
-            <h2 className="titleOrder">Resumen de la compra</h2>
-          </div>
-          {id ? <BuyAproduct id={id} /> : <ShoppingCart />}
-        </div>
       </div>
     </div>
   );
